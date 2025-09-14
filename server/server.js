98fe57955c1022e4b7033b432de82339a78b9cfa
Anish Singhal <anish.singhal0709@gmail.com>
2025-09-14 15:21:25 +0530

    Project

--- server/server.js
+++ server/server.js
@@ -1,19 +1,45 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
+const mongoose = require("mongoose");
 
 // Load environment variables from .env file
 dotenv.config();
+
+// Connect to MongoDB Atlas
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB Atlas:', error);
+  });
 
 // Create Express app
 const app = express();
 
 // Middlewares
 app.use(cors());
+
+// Enable CORS to allow requests from different domains or ports
+app.use(cors({
+  origin: '*',  // Or specify your frontend's domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true, // If you are dealing with cookies or authentication tokens
+}));
+
 app.use(express.json());
 
+// Static files access
+app.use(express.static(path.join(__dirname, '../client/build')));
+
 // Routes
-app.use("/api/v1/portfolio", require("./routes/portfolioRoute"));
+app.use('/api/v1/portfolio', require('./routes/portfolioRoute'));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // Port
 const PORT = process.env.PORT || 8080;
